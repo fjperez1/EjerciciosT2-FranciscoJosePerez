da0f59a30ce27f0227ba6cc487dd87d621c8a4cd
Ploxx <Ploxx@DESKTOP-1N2NPAT>
2022-10-24 22:32:33 +0200

    boletintema2

--- EjerciciosT2-FranciscoJosePerez/src/ejercicios/Ejercicio06.java
+++ EjerciciosT2-FranciscoJosePerez/src/ejercicios/Ejercicio06.java
@@ -6,21 +6,30 @@ public class Ejercicio06 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		//Declaramos las variables que vamos a utilizar
 		
 		int num1 = (int)(Math.random()*99)+1, num2 = (int)(Math.random()*99)+1, res;
+		/*
+		 * num1: en esta variable vamos a guardar el primer número aleatorio generado
+		 * num2: en esta variale vamos a guardar el segundo número aleatorio generado
+		 * res: en esta variable vamos a guardar el resultado de sumar el número 1 con el número 2 (el valor introducido por el usuario que juega)
+		 * 
+		 */
 		Scanner lectura = new Scanner (System.in);
 		
-		System.out.println("¿Cuál es el resultado de sumar " + num1 + " + " + num2 + " ?");
-		res = lectura.nextInt();
+		System.out.println("¿Cuál es el resultado de sumar " + num1 + " + " + num2 + " ?"); //Preguntamos al usuario cuál es el resultado de sumar los dos números
+		res = lectura.nextInt(); //Guardamos el valor introducido por el usuario en la variable "res"
 		
-		if (res==(num1+num2)) {
+		if (res==(num1+num2)) { //Comparamos el resultado introducido por el usuario con el resultado de sumar el número 1 + el número 2
 			
-			System.out.println("¡Resultado correcto!");
+			System.out.println("¡Resultado correcto!"); //Mostramos un mensaje por pantalla en caso de que la respuesta sea correcta
 			
 		} else
 			
-			System.out.println ("¡Resultado incorrecto!");
+			System.out.println ("¡Resultado incorrecto!"); //Mostramos otro mensaje distinto en caso de que la respuesta sea incorrecta
+		
+		//Cerramos el scanner
+		lectura.close();
 		
 		
 	}
